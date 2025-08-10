61a2ee13032274fc2b4f245244eeea18e716e42c
juliansalvador727 <juliansalvador727@gmail.com>
2025-08-10 16:39:36 -0700

    formatting

--- main.py
+++ main.py
@@ -3,30 +3,11 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 from datetime import datetime
 
-
-existing_file_names = [
-    "DSCN0003.JPG",
-    "DSCN0019.JPG",
-    "DSCN0237.JPG"
-]
-
-
 FOLDER = Path("images")
-
-image_paths = list({file for ext in ('*.jpg', '*.JPG') for file in FOLDER.glob(ext)})
+IMAGE_PATHS = list({file for ext in ('*.jpg', '*.JPG') for file in FOLDER.glob(ext)})
 
 new_file_names = []
-
-def unique_path(FOLDER, base_name, suffix):
-    new_name = f"{base_name}{suffix}"
-    candidate = FOLDER / new_name
-    count = 1
-    while candidate.exists():
-        candidate = FOLDER / f"{base_name}_{count}{suffix}"
-        count += 1
-    return candidate
-
-for img_path in image_paths:
+for img_path in IMAGE_PATHS:
     with Image.open(img_path) as image:
         exif_data = image._getexif()
         date_taken = None
@@ -44,9 +25,7 @@ for img_path in image_paths:
             new_file_names.append("unknown_date")
 
 name_counts = {}
-
 final_names = []
-
 for date_str in new_file_names:
     count = name_counts.get(date_str, 0)
     if count == 0:
@@ -56,7 +35,7 @@ for date_str in new_file_names:
     name_counts[date_str] = count + 1
     final_names.append(final_name)
 
-for file, base_name in zip(image_paths, final_names):
+for file, base_name in zip(IMAGE_PATHS, final_names):
     new_name = f"{base_name}{file.suffix}"
     new_path = file.parent / new_name
     print(f"Renaming {file.name} to {new_name}")
